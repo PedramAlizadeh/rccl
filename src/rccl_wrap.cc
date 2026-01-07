441a522820b33171a79f6e4042b38575e194d1f2
Pedram Alizadeh <pmohamma@amd.com>
2026-01-07 10:50:15 -0500

    Update rccl_wrap.cc

--- src/rccl_wrap.cc
+++ src/rccl_wrap.cc
@@ -47,7 +47,7 @@ RCCL_PARAM(ReducedCuEnable, "REDUCED_CU_ENABLE", 0);
 
 void rcclRestrictMaxChannels(struct ncclComm* comm, int& nc ) {
     if (comm->nNodes > 1 && IsArchMatch(comm->topo->nodes[GPU].nodes[0].gpu.gcn, "gfx950") && rcclParamReducedCuEnable() == 1)    {
-        nc = comm->nChannels = std::min(nc, 48);
+        comm->p2pnChannels = std::min(comm->nChannels, 16);
     }
 }
 
